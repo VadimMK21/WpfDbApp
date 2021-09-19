0726b04241c877d14f50e4f5ff5b0446e7bbce75
Vadim <bbrb2110@gmail.com>
2021-09-19 16:52:39 +0300

    Добавлена форма просмотра

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace WpfDbApp
@@ -14,11 +16,57 @@ namespace WpfDbApp
         {
             InitializeComponent();
 
-            userGrid.IsReadOnly = true;
+            //userGrid.IsReadOnly = true;
 
             db = new Context();
             db.Users.Load();
             userGrid.ItemsSource = db.Users.Local.ToBindingList();
-        }        
+        }
+
+        private void userGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            User user = userGrid.SelectedItem as User;
+
+            View view = new View(new User
+            {
+                id = user.id,
+                name = user.name,
+                adress = user.adress,
+                phone = user.phone,
+                
+            });
+
+            view.ShowDialog();
+                       
+
+            //User dr = userGrid.SelectedItem as User;
+
+            //if (dr.name != null)
+            //{
+            //    //View.tb_name.Text = dr.id.ToString();
+
+            //    //MessageBox.Show(dg.Items.IndexOf(dg.SelectedItem).ToString());            
+            //}
+
+
+
+
+        }
+        //private void btn_Del_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (userGrid.SelectedItems.Count > 0)
+        //    {
+        //        for (int i = 0; i < userGrid.SelectedItems.Count; i++)
+        //        {
+        //            User user = userGrid.SelectedItems[i] as User;
+        //            if (user != null)
+        //            {
+        //                db.Users.Remove(user);
+        //            }
+        //        }
+        //    }
+        //    db.SaveChanges();
+        //}
     }
 }
